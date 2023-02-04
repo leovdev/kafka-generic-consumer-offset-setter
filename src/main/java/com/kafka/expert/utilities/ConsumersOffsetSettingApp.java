87c7ea03ee7e20ffcc4aea2e182d929ebef82600
leonel vinasco <debi@MacBook-Air-de-leonel.local>
2023-02-04 02:52:10 -0500

    Initial hello world

---
+++ src/main/java/com/kafka/expert/utilities/ConsumersOffsetSettingApp.java
@@ -0,0 +1,13 @@
+package com.kafka.expert.utilities;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumersOffsetSettingApp {
+
+	public static void main(String[] args) {
+		System.out.println("hello java");
+
+	}
+
+}
